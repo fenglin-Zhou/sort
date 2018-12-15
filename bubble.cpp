081522ab08e764a132c0735ae3558397189e613c
fenglin <zfl869072773@gmail.com>
2018-12-15 20:19:23 +0800

    bubblesort

---
+++ bubble.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void bubble(int *a, int length)
+{
+    int temp;
+    for (int i = 0; i < length; i++)
+        for (int j = 0; j < length-i-1; j++)
+            if (a[j] >= a[j+1]) {
+                temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+            }
+}
+int main()
+{
+    int a[10] = {8, 7, 4, 3, 2, 1, 5, 6, 9, 10};
+    int length = sizeof(a)/sizeof(a[0]);
+    bubble(a, length);
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
