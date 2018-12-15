622113d1cb63b42525cdc5c49bc9d16bbf282e29
fenglin <zfl869072773@gmail.com>
2018-12-15 20:23:00 +0800

    bubblesort

--- bubble.cpp
+++ bubble.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void bubble(int *a, int length)
 {
     int temp;
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < length-1; i++)
         for (int j = 0; j < length-i-1; j++)
             if (a[j] >= a[j+1]) {
                 temp = a[j+1];
